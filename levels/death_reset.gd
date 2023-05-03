38f1714c5a4da2bdae56e37f852a1cbfa1fce96b
Jan Neumann <janneumannnneisen@gmail.com>
2023-05-03 14:11:38 +0200

    +fixed coin resetting
    Signed-off-by: Jan Neumann <janneumannnneisen@gmail.com>

--- levels/death_reset.gd
+++ levels/death_reset.gd
@@ -3,6 +3,11 @@ extends Area2D
 @onready var death_reset_sound = $death_reset_sound
 
 func _on_body_entered(body: CharacterBody2D):
+	reset_scores()
 	death_reset_sound.play()
 	await death_reset_sound.finished
 	get_tree().change_scene_to_file("res://menus/failure.tscn")
+
+func reset_scores():
+	Globals.reset_score()
+	Globals.reset_max_score()
